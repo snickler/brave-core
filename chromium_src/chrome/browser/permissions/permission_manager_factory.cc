4a455bf8edce27a2581e46a16af1d7e7a8f3e8e9
mkarolin <max@brave.com>
2020-03-31 12:55:26 -0400

    PermissionManager moved to //components.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/6b73c7497419485f3c9c01a620a8ad9c3621b60d
    
    commit 6b73c7497419485f3c9c01a620a8ad9c3621b60d
    Author: Clark DuVall <cduvall@chromium.org>
    Date:   Wed Mar 11 19:00:15 2020 +0000
    
        Move PermissionManager to //components/permissions
    
        The PermissionManagerFactory is kept in //chrome, and an equivalent will
        be added in //weblayer to initialize permission contexts. The map of
        permission contexts are now injected into the constructor of
        PermissionManager.
    
        Some unit tests were kept in //chrome which relate to chrome-specific
        logic in GetCanonicalOrigin().
    
        Bug: 1025609

---
+++ chromium_src/chrome/browser/permissions/permission_manager_factory.cc
@@ -0,0 +1,37 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/permissions/permission_manager_factory.h"
+
+#include <memory>
+
+#include "brave/browser/autoplay/autoplay_permission_context.h"
+#include "brave/browser/geolocation/brave_geolocation_permission_context.h"
+#include "components/content_settings/core/common/content_settings_types.h"
+
+namespace {
+
+// Forward declaration.
+permissions::PermissionManager::PermissionContextMap CreatePermissionContexts(
+    Profile* profile);
+
+permissions::PermissionManager::PermissionContextMap
+BraveCreatePermissionContexts(Profile* profile) {
+  permissions::PermissionManager::PermissionContextMap permission_contexts =
+      CreatePermissionContexts(profile);
+  permission_contexts[ContentSettingsType::AUTOPLAY] =
+      std::make_unique<AutoplayPermissionContext>(profile);
+  return permission_contexts;
+}
+
+}  // namespace
+
+#define BRAVE_PERMISSION_MANAGER_FACTORY_BUILD_SERVICE_INSTANCE_FOR \
+  BraveCreatePermissionContexts(profile)
+
+#define GeolocationPermissionContext BraveGeolocationPermissionContext
+#include "../../../../../chrome/browser/permissions/permission_manager_factory.cc"
+#undef GeolocationPermissionContext
+#undef BRAVE_PERMISSION_MANAGER_FACTORY_BUILD_SERVICE_INSTANCE_FOR
