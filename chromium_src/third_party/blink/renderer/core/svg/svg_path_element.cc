686894748369857fc84662e831c8fcbf710ce022
mkarolin <max@brave.com>
2020-04-02 18:00:26 -0400

    Updated blink svg patches with overrides.
    - svg_path_element.cc
    - svg_text_content_element.cc

---
+++ chromium_src/third_party/blink/renderer/core/svg/svg_path_element.cc
@@ -0,0 +1,14 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/renderer/brave_content_settings_agent_impl_helper.h"
+
+#define BRAVE_SVG_PATH_ELEMENT_GET_TOTAL_LENGTH         \
+  if (!AllowFingerprinting(GetDocument().GetFrame())) { \
+    return 0.0f;                                        \
+  }
+
+#include "../../../../../../third_party/blink/renderer/core/svg/svg_path_element.cc"
+#undef BRAVE_SVG_PATH_ELEMENT_GET_TOTAL_LENGTH
